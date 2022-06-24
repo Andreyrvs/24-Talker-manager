b2dde5ca116f3b34b507092d147468606130dda1
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-24 14:12:37 +0000

    Chore: (Requisito 01) - Cria rota talker com metodo 'GET'

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises')
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,15 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+async function palestrantes() {
+  const fileContent = await fs.readFile('./talker.json', 'utf-8')
+  return JSON.parse(fileContent)
+}
+
+app.get('/talker', async (req, res) => {
+  res.status(200).json(await palestrantes())
+})
 
 app.listen(PORT, () => {
   console.log('Online');
