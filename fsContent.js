124d574cbe284a4bcd9b915fec6cd9ce7eb048cc
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-24 23:48:36 +0000

    Fix: Adiciona Try/Catch na leitura e escrita do 'talker.json'

--- fsContent.js
+++ fsContent.js
@@ -3,13 +3,22 @@ const fs = require('fs/promises');
 const FILE_NAME = './talker.json';
 
 async function getSpeaker() {
-  const fileContent = await fs.readFile(FILE_NAME, 'utf-8');
-  return JSON.parse(fileContent);
+  try {
+    const fileContent = await fs.readFile(FILE_NAME, 'utf-8');
+    return JSON.parse(fileContent);
+  } catch (error) {
+    console.error(`Não foi possivel ler o arquivo ${FILE_NAME}\n Erro: ${error}`);
+    process.exit(1);
+  }
 }
 
 async function setSpeaker(newTalker) {
-  const fileContent = await fs.writeFile(FILE_NAME, JSON.stringify(newTalker));
-  return fileContent;
+  try {
+    await fs.writeFile(FILE_NAME, JSON.stringify(newTalker));
+    console.log('Arquivo escrito com sucesso!');
+  } catch (error) {
+    console.error(`Erro ao escrever o arquivo: ${error.message}`);
+  }
 }
 
 module.exports = { getSpeaker, setSpeaker };
