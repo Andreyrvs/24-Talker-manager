b5b81d7cf2f8197367b56d0c62990060034ba6c5
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-24 11:15:37 -0300

    Fix: (eslint) - Adiciona 'semilicon' que estavam faltando

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs/promises')
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,13 +14,13 @@ app.get('/', (_request, response) => {
 });
 
 async function palestrantes() {
-  const fileContent = await fs.readFile('./talker.json', 'utf-8')
-  return JSON.parse(fileContent)
+  const fileContent = await fs.readFile('./talker.json', 'utf-8');
+  return JSON.parse(fileContent);
 }
 
 app.get('/talker', async (req, res) => {
-  res.status(200).json(await palestrantes())
-})
+  res.status(200).json(await palestrantes());
+});
 
 app.listen(PORT, () => {
   console.log('Online');
