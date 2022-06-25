da486fb7b05b03f42a9cedb84e0e891d3b51a87a
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-06-25 00:26:13 +0000

    Fix: (Middleware autenticacao) - muta a regra de negocio para contemplar o 'Token invalido', Troca o retorno das validacoes para corresponderem a regra de negocio usada

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,11 +4,11 @@ module.exports = (req, res, next) => {
   // console.log('TOKEN', token);
   // console.log('AUTHORIZATION', authorization);
   if (!authorization) {
-    return res.status(401).json({ message: 'Token inválido' });
+    return res.status(401).json({ message: 'Token não encontrado' });
   }
   
-  if (authorization.length !== 16) {
-    return res.status(401).json({ message: 'Token não encontrado' });
+  if (authorization.length < 16 || authorization.length > 16) {
+    return res.status(401).json({ message: 'Token inválido' });
   }
 
   next();
